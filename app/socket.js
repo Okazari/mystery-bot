90e661c2b987a1bf57d11e49f1ae66a98a5c2d52
acailly <antoine.cailly@zenika.com>
2017-01-10 00:10:05 +0100

    Ajout de vorpal comme interface entre le bot et les points d'accès externes (CLI, Socket...)

--- app/socket.js
+++ app/socket.js
@@ -1,15 +1,15 @@
 const log = (msg) => console.log('[SOCKET]', msg)
-const mapping = require('./bot.json')
+const bot = require('./bot')
 module.exports = (io) => {
   io.on('connection', (socket => {
     log('CONNECT')
     socket.on('disconnect', () => log('DISCONNECT'))
     socket.on('chat message', function(request) {
-      log(`I received : ${request.message}`)
-      const message = request.message.toLowerCase()
-      if(mapping[message]) {
-        socket.emit('chat response', { message: mapping[message] })
-        log(`I answered : ${mapping[message]}`)
+      log(`I received : ${request.message}`)      
+      const response = bot.send(`message ${request.message}`)
+      if(response) {
+        socket.emit('chat response', { message: response })
+        log(`I answered : ${response}`)
       }
     })
   }))
