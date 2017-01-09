fff5688aff2011f01356cb621590d3559ed986ff
Okazari <plouzennec.benjamin@gmail.com>
2017-01-10 00:54:31 +0100

    First try for style

--- public/index.js
+++ public/index.js
@@ -6,16 +6,24 @@ document.addEventListener('DOMContentLoaded', function(){
   form.addEventListener('submit', function(event){
     event.preventDefault()
     socket.emit('chat message', { message: event.target.message.value })
-    var newdiv = document.createElement('div')
-    newdiv.innerText = "Moi : " + event.target.message.value
-    chat.appendChild(newdiv)
+    var container = document.createElement('div')
+    var newdiv = document.createElement('span')
+    container.className = 'message-container'
+    newdiv.className = 'message me'
+    newdiv.innerText = event.target.message.value
+    container.appendChild(newdiv)
+    chat.appendChild(container)
     event.target.message.value = ""
   })
 
   socket.on('chat response', function(response) {
-    var newdiv = document.createElement('div')
-    newdiv.innerText = "Mystery bot : " + response.message
-    chat.appendChild(newdiv)
+    var container = document.createElement('div')
+    var newdiv = document.createElement('span')
+    container.className = 'message-container bot-container'
+    newdiv.className = 'message bot'
+    newdiv.innerText = response.message
+    container.appendChild(newdiv)
+    chat.appendChild(container)
   })
 
 })
