ce168a09bcec4311fa74a529e57ad9a4291ff9d3
acailly <antoine.cailly@zenika.com>
2017-01-10 23:04:44 +0100

    feat - Add a brain that exposes some RiveScript features

--- app/bot/index.js
+++ app/bot/index.js
@@ -1,8 +1,10 @@
 const vorpal = require('vorpal')()
 const chalk = vorpal.chalk
 const simpleConfig = require('./simpleConfig')
+const rivescript = require('./rivescript')
 
 vorpal.use(simpleConfig)
+vorpal.use(rivescript)
     
 vorpal
 	.delimiter(chalk.magenta('mystery-bot~$'))
