aa34b5cca1feba73a9e33a8879af68f462fb6fbb
Okazari <plouzennec.benjamin@gmail.com>
2017-02-21 22:23:58 +0100

    Updated but will only work with my own fork of botpress-rivescripto

--- actions/test.js
+++ actions/test.js
@@ -1,31 +1,20 @@
-const customResponseHandler = (bp, event, text, options) => {
-  let sender, sendintTo
-  if (event.platform === 'facebook') {
-    sender = bp.messenger
-    sendingTo = event.user.id
-  } else if (event.platform === 'discord') {
-    sender = bp.discord
-    sendingTo = event.channel.id
-  }
-  sender.sendText(sendingTo, text, options)
-}
-
-module.exports = (bp, rs, event) => {
+module.exports = (send) => {
   //doAyncStuff.then(() => {
-      options = {
-          quick_replies: [
-            {
-              content_type :"TEXT",
-              title: "Next please",
-              payload: "Next please"
-            },
-            {
-              content_type :"TEXT",
-              title: "Previous please",
-              payload: "Previous please"
-            }
-          ]
-        }
-      customResponseHandler(bp, event, "Heyyah !", options)
+      console.log('coucou')
+      const options = {
+        quick_replies: [
+          {
+            content_type :"TEXT",
+            title: "Next please",
+            payload: "Next please"
+          },
+          {
+            content_type :"TEXT",
+            title: "Previous please",
+            payload: "Previous please"
+          }
+        ]
+      }
+      send("Heyyah !", options)
   //})
 }
