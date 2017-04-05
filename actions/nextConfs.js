e099c4d328e2a3356a3f8d92af2d4b1ee7cfc0c5
acailly <antoine.cailly@zenika.com>
2017-04-05 21:26:15 +0200

    :sparkles: Added search for dates

--- actions/nextConfs.js
+++ actions/nextConfs.js
@@ -5,6 +5,7 @@ const currentDate = require('./currentDate')
 const formatConfList = require('./formatConfList')
 const formatDate = require('./formatDate')
 const extractStartDate = require('./extractStartDate')
+const defaultMenu = require('./defaultMenu')
 
 const happensNext = R.curry(
   (referenceDate, confDate) => {
@@ -19,17 +20,25 @@ const nextConfs = eventStartUnixTime => {
   const referenceDate = eventStartUnixTime ? moment.unix(eventStartUnixTime) : currentDate()
   const formatedReferenceDate = formatDate(referenceDate)
 
-  return [
-    `Voici les conférences qui démarrent aux environs de ${formatedReferenceDate}`,
-    formatConfList(
-      confs
+  const foundConfs = confs
         .filter(
           R.pipe(
             extractStartDate,
             happensNext(referenceDate)
           )
         )
-    )]
+
+  if (foundConfs.length) {
+    return [
+      `Voici les conférences qui démarrent aux environs de ${formatedReferenceDate}`,
+      formatConfList(foundConfs)
+    ]
+  } else {
+    return [
+      `Je n'ai rien trouvé aux environs de ${formatedReferenceDate}`,
+      defaultMenu()
+    ]
+  }
 }
 
 module.exports = nextConfs
--- actions/searchConfs.js
+++ actions/searchConfs.js
@@ -1,11 +1,16 @@
 const R = require('ramda')
-const confs = require('../breizhcamp.json')
-const formatConfList = require('./formatConfList')
-const defaultMenu = require('./defaultMenu')
+const chrono = require('chrono-node')
+const moment = require('moment')
 
 const lunr = require('lunr')
 require('lunr-languages/lunr.stemmer.support')(lunr)
 require('lunr-languages/lunr.fr')(lunr)
+
+const formatConfList = require('./formatConfList')
+const defaultMenu = require('./defaultMenu')
+const currentDate = require('./currentDate')
+const nextConfs = require('./nextConfs')
+const confs = require('../breizhcamp.json')
 
 const index = lunr(function () {
   this.use(lunr.fr)
@@ -16,6 +21,14 @@ const index = lunr(function () {
 confs.forEach(conf => index.add(conf))
 
 const searchConfs = search => {
+  // Search for date
+  const date = chrono.parseDate(search, currentDate())
+  if (date) {
+    const unixTime = moment(date).unix()
+    return nextConfs(unixTime)
+  }
+
+  // Search for keyword
   const results = index.search(search)
   const foundConfIds = R.pluck('ref', results)
   const foundConfs = R.filter(conf => R.contains(conf.id, foundConfIds), confs)
