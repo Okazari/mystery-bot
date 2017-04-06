9c5e7c9eb48a5cfd0cea8fa9aa6275c475e137c1
acailly <antoine.cailly@zenika.com>
2017-04-07 00:52:14 +0200

    :mag: EACCESS error on Azure, try with port 8080

--- botpress-botbuilder/index.js
+++ botpress-botbuilder/index.js
@@ -63,7 +63,7 @@ module.exports = {
 
   ready: function (bp) {
     const server = restify.createServer()
-    server.listen(process.env.port || process.env.PORT || 80, function () {
+    server.listen(process.env.port || process.env.PORT || 8080, function () {
       console.log('%s listening to %s', server.name, server.url)
     })
 
