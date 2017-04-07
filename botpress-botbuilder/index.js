77531c4071d65c6d10e185bbd7b2007dd54b6d89
acailly <antoine.cailly@ext.biomerieux.com>
2017-04-07 10:29:18 +0200

    :mag: Retry with 80

--- botpress-botbuilder/index.js
+++ botpress-botbuilder/index.js
@@ -63,7 +63,7 @@ module.exports = {
 
   ready: function (bp) {
     const server = restify.createServer()
-    server.listen(process.env.port || process.env.PORT || 8083, function () {
+    server.listen(process.env.port || process.env.PORT || 80, function () {
       console.log('%s listening to %s', server.name, server.url)
     })
 
