1abd64c3c4cf70b30fa1cbe46ff85fb2fc704dda
Plouzennec Benjamin <benjamin.plouzennec@zenika.com>
2017-04-19 14:48:51 +0200

    Added some new behavior for nextConfs

--- actions/currentDate.js
+++ actions/currentDate.js
@@ -2,7 +2,7 @@ const moment = require('moment')
 require('moment-timezone')
 
 module.exports = function () {
-  // return moment('2017-04-21T13:00:00.000')
+  // return moment('2017-04-19T14:11:00.000')
   return moment().add(2, 'h')
   // return moment().tz('Europe/Paris')
 }
--- actions/nextConfs.js
+++ actions/nextConfs.js
@@ -12,11 +12,73 @@ const breizhcampStartDate = moment('2017-04-19T10:00:00.000')
 const happensNext = R.curry(
   (referenceDate, confDate) => {
     return confDate.isBetween(
-      moment(referenceDate).subtract(15, 'm'),
-      moment(referenceDate).add(45, 'm')
+      moment(referenceDate).subtract(10, 'm'),
+      moment(referenceDate).add(15, 'm')
     )
   }
 )
+
+const isAfter = R.curry(
+  (referenceDate, confDate) => {
+    return confDate.isAfter(moment(referenceDate).subtract(10, 'm'  ))
+  }
+)
+
+const getSoonestConfs = (referenceDate) => {
+  const nextConfs = confs.filter(
+      R.pipe(
+        extractStartDate,
+        isAfter(referenceDate)
+      )
+    )
+  if (!nextConfs.length) {
+    return [
+      `Le breizhcamp c'est fini pour cette année :'(`,
+      defaultMenu(),
+    ]
+  }
+  const soonestConf = nextConfs.sort((a,b) => a.event_start > b.event_start ? 1 : -1).shift()
+  const soonestConfDate = moment(soonestConf.event_start)
+  const foundConfs = confs.filter(
+    R.pipe(
+      extractStartDate,
+      happensNext(soonestConfDate)
+    )
+  )
+  const startedConfs = soonestConfDate.isBefore(moment(referenceDate))
+  let message = `Prochaine série de conférences: ${formatDate(soonestConfDate)}`
+  if(startedConfs) {
+    message = `Ces conférences ont commencé il y a moins de 10min`
+  }
+  const formattedConfs = formatConfList(foundConfs)
+  return [
+    message,
+    ...formattedConfs
+  ]
+}
+
+const searchConfByDate = (referenceDate) => {
+  const formatedReferenceDate = formatDate(referenceDate)
+  const foundConfs = confs.filter(
+    R.pipe(
+      extractStartDate,
+      happensNext(referenceDate)
+    )
+  )
+  console.log('FOUNDCONFS', foundConfs)
+  if(!foundConfs.length) {
+    return [
+      `Je n'ai rien trouvé aux environs de ${formatedReferenceDate}`,
+      defaultMenu(),
+    ]
+  } else {
+    const formattedConfs = formatConfList(foundConfs)
+    return [
+      `Voici les conférences qui démarrent aux environs de ${formatedReferenceDate}`,
+      ...formattedConfs
+    ]
+  }
+}
 
 const nextConfs = eventStartUnixTime => {
   const referenceDate = eventStartUnixTime ? moment.unix(eventStartUnixTime) : currentDate()
@@ -30,27 +92,12 @@ const nextConfs = eventStartUnixTime => {
   //   ]
   // }
 
-  const formatedReferenceDate = formatDate(referenceDate)
-
-  const foundConfs = confs
-        .filter(
-          R.pipe(
-            extractStartDate,
-            happensNext(referenceDate)
-          )
-        )
-
-  if (foundConfs.length) {
-    const formattedConfs = formatConfList(foundConfs)
-    return [
-      `Voici les conférences qui démarrent aux environs de ${formatedReferenceDate}`,
-      ...formattedConfs
-    ]
+  // En cas de recherche explicite sans résultats
+  if (eventStartUnixTime) {
+    return searchConfByDate(referenceDate)
+  // Sinon on renvoie la prochaine série de conférences
   } else {
-    return [
-      `Je n'ai rien trouvé aux environs de ${formatedReferenceDate}`,
-      defaultMenu()
-    ]
+    return getSoonestConfs(referenceDate)
   }
 }
 
